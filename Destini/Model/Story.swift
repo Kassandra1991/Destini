75c81e05428370e2174e87fd32de76fef7bf0264
Alex Asichka <sanechka3@mail.ru>
2023-02-02 12:25:35 +0200

    reorganization structure files

---
+++ Destini/Model/Story.swift
@@ -0,0 +1,8 @@
+//
+//  Story.swift
+//  Destini
+//
+//  Created by Aleksandra Asichka on 2023-02-02.
+//
+
+import Foundation
---
+++ Destini/Model/StoryBrain.swift
@@ -0,0 +1,8 @@
+//
+//  StoryBrain.swift
+//  Destini
+//
+//  Created by Aleksandra Asichka on 2023-02-02.
+//
+
+import Foundation
